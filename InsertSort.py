afd2a22188d5c0a1c07d03e63521956438d50bd1
Manish Pandey <51872315+manish806@users.noreply.github.com>
2020-08-17 19:58:52 +0530

    Add files via upload

---
+++ InsertSort.py
@@ -0,0 +1,36 @@
+
+'''
+Pseudocode
+INSERTION-SORT(A)
+   for i = 1 to n
+   	key ← A [i]
+    	j ← i – 1
+  	 while j > = 0 and A[j] > key
+   		A[j+1] ← A[j]
+   		j ← j – 1
+   	End while
+   	A[j+1] ← key
+  End for
+'''
+
+def insertionSort(myarr):
+    for i in range(1,len(myarr)):
+        key = myarr[i]
+        #Move the element myarr[0,i-1] that are greater than key to 1 position from their current position.
+        j= i-1
+        while j >=0 and key < myarr[j]:
+            myarr[j+1] = myarr[j]
+            j -=1
+        #Place the element
+        myarr[j+1] = key
+if __name__== "__main__":
+    # Driver code to test above
+    arr = [12, 11, 13, 5, 6, 9]
+    print("Input Array is :",end=' ')
+    for i in range(len(arr)):
+        print(arr[i],end=' ')
+    print(" ")
+    insertionSort(arr)
+    print ("Sorted array is :",end=' ')
+    for i in range(len(arr)):
+        print (arr[i], end=' ')
